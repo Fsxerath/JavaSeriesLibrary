9588e9296cc5d511d29f43e5ce6f5d9d22a493b3
Fsxerath <frediangarita1999@gmail.com>
2024-09-21 16:13:12 -0500

    feat (application util) add constants to createTypeSeries dto

---
+++ src/main/java/com/example/series_library_java/application/util/CreateTypeSeriesConstants.java
@@ -0,0 +1,12 @@
+package com.example.series_library_java.application.util;
+
+public class CreateTypeSeriesConstants {
+    public CreateTypeSeriesConstants() {}
+
+    public static final String TYPE_IS_MANDATORY = "Name is Mandatory";
+    public static final String DESCRIPTION_IS_MANDATORY = "Description is Mandatory";
+    public static final int TYPE_MAX_LENGTH = 100;
+    public static final int TYPE_MIN_LENGTH = 3;
+    public static final int DESCRIPTION_MAX_LENGTH = 255;
+    public static final int DESCRIPTION_MIN_LENGTH = 10;
+}
