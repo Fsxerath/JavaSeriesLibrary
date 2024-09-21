b4e3c995f59c04aabf5e851e78d7d7a39815ba37
Fsxerath <frediangarita1999@gmail.com>
2024-09-21 15:47:25 -0500

    refactor(TypeSeries) change id type UUID to Long

--- src/main/java/com/example/series_library_java/domain/model/TypeSeries.java
+++ src/main/java/com/example/series_library_java/domain/model/TypeSeries.java
@@ -1,21 +1,21 @@
 package com.example.series_library_java.domain.model;
 
 public class TypeSeries {
-    private long id;
+    private Long id;
     private String type;
     private String description;
 
-    public TypeSeries(long id, String type, String description) {
+    public TypeSeries(Long id, String type, String description) {
         this.id = id;
         this.type = type;
         this.description = description;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/com/example/series_library_java/infrastructure/driven/jpa/postgresql/entity/TypeSeriesEntity.java
+++ src/main/java/com/example/series_library_java/infrastructure/driven/jpa/postgresql/entity/TypeSeriesEntity.java
@@ -1,17 +1,26 @@
 package com.example.series_library_java.infrastructure.driven.jpa.postgresql.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "type_series")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TypeSeriesEntity {
     @Id
     @GeneratedValue()
-    private long id;
-    @Column(length = 100)
+    private Long id;
+    @Column( length = 100)
     private String type;
+    @Column()
     private String description;
     @OneToMany(mappedBy = "type")
     private List<SeriesEntity> seriesEntity;
